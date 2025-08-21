056b17814324389de96c71279c26f5db1e8fc895
Viktor <vikt.sh@gmail.com>
2025-08-21 22:13:42 +0300

    Initial commit

---
+++ src/main/java/org/example/shvidkiyhomework_int4/ShvidkiyHomeworkInt4Application.java
@@ -0,0 +1,13 @@
+package org.example.shvidkiyhomework_int4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShvidkiyHomeworkInt4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShvidkiyHomeworkInt4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shvidkiyhomework_int4/ShvidkiyHomeworkInt4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shvidkiyhomework_int4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShvidkiyHomeworkInt4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
